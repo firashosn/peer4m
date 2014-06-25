ac182e0051473a0bb33977a32f59923bf10f6d02
rmarinac89 <rmarinac@icloud.com>
2014-06-24 20:56:21 -0400

    added changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def after_sign_in_path_for
+    
+    if @user.role = "instructor"
+    new_courses_path
+    
+    elsif @user.role = "student"
+    course_path
+    
+    end
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140623020758) do
+ActiveRecord::Schema.define(version: 20140624005331) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -80,7 +80,6 @@ ActiveRecord::Schema.define(version: 20140623020758) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
