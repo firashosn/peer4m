e21f860162c7727b0f05901e85ad7d72db361375
rmarinac89 <rmarinac@icloud.com>
2014-08-10 01:09:00 -0400

    fixed bg issue

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -56,19 +56,19 @@ $('.collapse').collapse()
 
 // Dropdown Menu caret rotates
 
-$(document).ready(function() {
-	var position = 0;
-	$('.dropdown-toggle').click(function() {
-		position+=180;
-		$('.caret').css({
-			'-webkit-transform':'rotate('+position+'deg)',
-			'-moz-transform':'rotate('+position+'deg)',
-			'-ms-transform':'rotate('+position+'deg)',
-			'-o-transform':'rotate('+position+'deg)'
-		});
-	});
+// $(document).ready(function() {
+// 	var position = 0;
+// 	$('.dropdown-toggle').click(function() {
+// 		position+=180;
+// 		$('.caret').css({
+// 			'-webkit-transform':'rotate('+position+'deg)',
+// 			'-moz-transform':'rotate('+position+'deg)',
+// 			'-ms-transform':'rotate('+position+'deg)',
+// 			'-o-transform':'rotate('+position+'deg)'
+// 		});
+// 	});
 
-});
+// });
 
 
 
