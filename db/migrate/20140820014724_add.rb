ee926dcda7632109c06039b7d182201d46f7df4c
rmarinac89 <rmarinac@icloud.com>
2014-08-26 22:44:34 -0400

    fixed migration issue

--- db/migrate/20140820014724_add.rb
+++ db/migrate/20140820014724_add.rb
@@ -1,5 +1,5 @@
-# class Add < ActiveRecord::Migration
-#   def change
-#     add_column :users, :current_institution, :string
-#   end
-# end
+class Add < ActiveRecord::Migration
+  def change
+    add_column :users, :current_institution, :string
+  end
+end
