4f2372b520f38eb18f5d5194a2a7899de55b4da0
firashosn <firashosn@gmail.com>
2014-09-02 22:26:53 -0400

    batch notification
    batch notification

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -3,8 +3,11 @@ class Assignment < ActiveRecord::Base
   has_many :teams
 
 def is_open_for_evaluation
-  return self.close_time < Time.current 
-  #&& self.open_time > Time.current
+  return self.close_time < Time.current #&& self.open_time > Time.current
+end
+
+def get_closing_time_day_difference
+  return (self.close_time - Time.current).to_i / 1.day 
 end
 
 def update_notification
--- app/models/course.rb
+++ app/models/course.rb
@@ -10,6 +10,19 @@ class Course < ActiveRecord::Base
   def get_all_assignments
     return self.assignments 
   end
+
+  def get_open_assignments
+  	my_assignments = get_all_assignments()
+  	open_assignments = []
+  	if my_assignments != nil && my_assignments.count > 0
+  		my_assignments.each do | assignment |
+  			if assignment.is_open_for_evaluation() 
+  				open_assignments.push(assignment)
+  			end
+  		end
+  	end
+  	return open_assignments
+  end
   
 end
 
